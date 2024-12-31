6d5fbed34801c8b61d6f1fe0db7350ccd3d3fae8
deine.kleine.liebe@gmail.com <ms.balashova.2001@gmail.com>
2024-12-31 18:58:45 +0300

    Fix validation fails status

--- backend/src/controllers/products.ts
+++ backend/src/controllers/products.ts
@@ -49,11 +49,6 @@ export const addProduct = async (req: Request, res: Response, next: NextFunction
           ),
         );
       }
-      if (error instanceof Error && error.message.includes('Validation failed')) {
-        return next(
-          new BadRequestError(error.message),
-        );
-      }
       return next(error);
     });
 };
--- backend/src/middlewares/error-handler.ts
+++ backend/src/middlewares/error-handler.ts
@@ -1,14 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
-import BadRequestError from '../errors/bad-request-error';
-import ConflictError from '../errors/conflict-error';
-import NotFoundError from '../errors/not-found-error';
 
 const errorHandler = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
-  if (
-    err instanceof BadRequestError
-    || err instanceof NotFoundError
-    || err instanceof ConflictError
-  ) {
+  if (err.message.includes('Validation failed')) {
+    return res.status(400).send({ message: err.message });
+  }
+  if ('statusCode' in err && err.statusCode && typeof err.statusCode === 'number') {
     return res.status(err.statusCode).send({ message: err.message });
   }
   return res.status(500).send({ message: err.message });
